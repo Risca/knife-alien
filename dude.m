4cf73ef657066191fb00ad90de2e11f9a2589f9a
Staffan Sjoqvist <stafan87@gmail.com>
2011-04-14 16:57:00 +0200

    Created an experimental and totally useless class :-)

---
+++ dude.m
@@ -0,0 +1,9 @@
+% This is just Staffan experimenting with classes...
+
+classdef dude	% Class describing an average dude.
+    properties
+        length	% Length of dude in meters
+		weight	% Weight of dude in kilos
+	end
+	% no methods implemented yet!
+end
