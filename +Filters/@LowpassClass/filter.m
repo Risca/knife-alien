4e4cc15c803b97f955bdbe5e23049406fbb4db26
Patrik Dahlström <patda293@student.liu.se>
2011-05-03 16:52:03 +0200

    Ändrade i lågpassfiltret
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- +Filters/@LowpassClass/filter.m
+++ +Filters/@LowpassClass/filter.m
@@ -1,6 +1,6 @@
 function filteredData = filter (obj, data)
     % Make filtering
-    obj.Data = data.*2;
+    obj.Data = data./2;
     filteredData = obj.Data;
     % Notify world
     notify(obj,'FilteringComplete');
