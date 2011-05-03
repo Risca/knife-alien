3fd4222f085aa2463df42400b0ff4093e83f1ef8
Patrik Dahlström <patda293@student.liu.se>
2011-05-03 17:43:33 +0200

    Kommer inte ihåg vad jag ändrade
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- filter_test.m
+++ filter_test.m
@@ -1,10 +1,30 @@
+%% Init
+clear
 import Filters.*
 dataToBeFiltered = [1 2 3 4 5];
 
+%% Do som lowpass filtering
 obj = LowpassFilter();
 obj.filter(dataToBeFiltered);
-filteredData = obj.Data
+obj.Data
 
+%% Do some highpass filtering
 obj = HighpassFilter();
-obj.filter(filteredData);
-filteredData = obj.Data
+obj.filter(dataToBeFiltered);
+obj.Data
+
+%% Let's do this with events
+clear obj
+obj1 = LowpassFilter();
+obj2 = HighpassFilter();
+addlistener(obj1,'FilteringComplete',@filterEventHandler);
+addlistener(obj2,'FilteringComplete',@filterEventHandler);
+% addlistener(eventObject,'EventName',@Obj.methodName) % — for a method of Obj.
+% addlistener(eventObject,'EventName',@ClassName.methodName) % — for a static method of the class ClassName.
+
+%%
+% Do some lowpass filtering
+obj1.filter(dataToBeFiltered);
+
+% Do some highpass filtering
+obj2.filter(dataToBeFiltered);
