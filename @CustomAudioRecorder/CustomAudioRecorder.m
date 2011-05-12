e3a83f8ed9b5e4b2a9445d70f6323687395e721b
Patrik Dahlström <patda293@student.liu.se>
2011-05-12 09:55:40 +0200

    Utökade CustomAudioRecorder, men har problem med att ställa in timerFcn
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- @CustomAudioRecorder/CustomAudioRecorder.m
+++ @CustomAudioRecorder/CustomAudioRecorder.m
@@ -1,10 +1,18 @@
 classdef CustomAudioRecorder < audiorecorder & handle
     properties (SetAccess = protected)
         Data
+        fftData
     end
     methods
-        function obj = CustomAudioRecorder(Fs,nBits,nChannels)
+        function obj = CustomAudioRecorder(Fs,nBits,nChannels,Nfft)
             obj = obj@audiorecorder(Fs,nBits,nChannels);
+            obj.fftData.fs = Fs;
+            obj.fftData.Nfft = Nfft;
+%             set(obj, ...
+%                 'TimerFcn',@audioTimerFcn, ...
+%                 'TimerPeriod', 0.1, ...
+%                 'UserData', fftData);
+            obj.TimerFcn = @timerFcn;
         end
     end
     events
---
+++ @CustomAudioRecorder/timerFcn.m
@@ -0,0 +1,12 @@
+function timerFcn(obj,eventData)
+obj.TotalSamples;
+data = obj.UserData;
+Nfft = data.Nfft;
+audioData = getaudiodata(obj);
+% Only process last Nfft samples
+audioData = audioData(end-Nfft:end);
+Y = fft(audioData,Nfft);
+Y = Y(1:Nfft/2);
+Y = abs(Y)*2/Nfft;
+set(data.stemHandle,'YData',Y);
+drawnow;
