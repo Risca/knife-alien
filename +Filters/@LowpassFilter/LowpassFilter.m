dbb8a8f37e1057954dcc19473e32014d552973eb
Staffan Sjöqvist <stafan87@gmail.com>
2011-05-12 16:30:05 +0200

    Lade till funktion för att ställa in gränsfrekvens
    Signed-off-by: Staffan Sjöqvist <stafan87@gmail.com>

--- +Filters/@LowpassFilter/LowpassFilter.m
+++ +Filters/@LowpassFilter/LowpassFilter.m
@@ -1,6 +1,16 @@
 classdef LowpassFilter < Filters.FilterClass & handle
+    properties (SetAccess = protected)
+        CutOffFreq
+    end
+    
     methods
         function obj = LowpassFilter()
         end
+        
+        function setCutoffFreq(obj, freq)
+            obj.CutOffFreq = freq;
+        end
+        
+        
     end
 end
