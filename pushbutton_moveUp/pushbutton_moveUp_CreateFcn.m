52a6e2ff1ab9f031d3b79a62a7c9acfdd008d6f5
Daniel Josefsson <danjo140@student.liu.se>
2011-05-12 16:31:32 +0200

    Tog bort den föråldrade mappen pushbutton_moveUp och dess innehåll.
    Signed-off-by: Daniel Josefsson <danjo140@student.liu.se>

--- pushbutton_moveUp/pushbutton_moveUp_CreateFcn.m
+++
@@ -1,8 +0,0 @@
-% --- Executes during object creation, after setting all properties.
-function pushbutton_moveUp_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to pushbutton_moveUp (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-%Disable from start since there is no added filters
-set(hObject, 'Enable', 'off');
