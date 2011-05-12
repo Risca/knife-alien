49702051b7cd7cd8f4324efb157310cbf89dabf5
Patrik Dahlström <patda293@student.liu.se>
2011-05-13 00:39:51 +0200

    CustomAudioRecorder will now transform audio data before notifying NewAudioData
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- @CustomAudioRecorder/CustomAudioRecorder.m
+++ @CustomAudioRecorder/CustomAudioRecorder.m
@@ -1,5 +1,6 @@
 classdef CustomAudioRecorder < audiorecorder & handle
     properties (SetAccess = protected)
+        Data
         fftData
         stemHandle
     end
@@ -12,7 +13,15 @@ classdef CustomAudioRecorder < audiorecorder & handle
             obj.TimerFcn = @obj.customTimerFcn;
         end
         function customTimerFcn(obj,src,eventData)
-            notify(src,'NewAudioData');
+            Nfft = obj.fftData.Nfft;
+            audioData = getaudiodata(src);
+            % Only process last Nfft samples
+            obj.Data = audioData(end-Nfft:end);
+            obj.Data = fft(audioData,Nfft);
+            obj.Data = obj.Data(1:Nfft/2);
+            % Adjust magnitude
+            obj.Data = abs(obj.Data)*2/Nfft;
+            notify(obj,'NewAudioData');
         end
     end
     methods (Static,Hidden)
--- audioTimerFcn.m
+++ audioTimerFcn.m
@@ -1,10 +1,3 @@
 function audioTimerFcn(obj,eventData)
-Nfft = obj.fftData.Nfft;
-audioData = getaudiodata(obj);
-% Only process last Nfft samples
-audioData = audioData(end-Nfft:end);
-Y = fft(audioData,Nfft);
-Y = Y(1:Nfft/2);
-Y = abs(Y)*2/Nfft;
-set(obj.stemHandle,'YData',Y);
+set(obj.stemHandle,'YData',obj.Data);
 drawnow;
