48a476845f4b1fd9aa48a2645d44a3631b9aea87
Daniel Josefsson <danjo140@student.liu.se>
2011-05-12 16:34:47 +0200

    Tog bort den föråldrade mappen listbox_activeFilters och dess innehåll.
    Signed-off-by: Daniel Josefsson <danjo140@student.liu.se>

--- listbox_activeFilters/listbox_activeFilters_Callback.m
+++
@@ -1,6 +0,0 @@
-% --- Executes on selection change in listbox_activeFilters.
-function listbox_activeFilters_Callback(hObject, eventdata, handles)
-% hObject    handle to listbox_activeFilters (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-updateMoveFilterButtons(handles);
