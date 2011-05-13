05c6245f92f17a2a41174d609346844ad1925cc3
Staffan Sjöqvist <stafan87@gmail.com>
2011-05-13 11:17:36 +0200

    Fixade lite småfel och snyggade till koden
    Signed-off-by: Staffan Sjöqvist <stafan87@gmail.com>

--- +Filters/@LowpassFilter/filter.m
+++ +Filters/@LowpassFilter/filter.m
@@ -1,36 +1,19 @@
 function filteredData = filter (obj, data)
     
     % Data is assumed to be transformed.
-    
-
     % Sample frequency hard-coded for now
     fs = 22050;
       
-    
-    % Calculate which parts of data to set to zero.
+    % Calculate which parts of data to set to zero and create filtering mask
     delta_f = fs / length(data);
-    
-    % Create filtering mask
     n = floor(obj.CutOffFreq / delta_f);
     
     if n > length(data)
         n = length(data);
     end
-    
     filteringMask = [ ones(1,n) zeros(1,(length(data)-n)) ];
     
     % Filter data:
-   data = data .* filteringMask;
-   filteredData = data;
-    
-
-
-
-    
-
-%     % Make filtering
-%     obj.Data = data./2
-%     filteredData = obj.Data;
-%     % Notify world
-%     notify(obj,'FilteringComplete');
+    obj.Data = data .* filteringMask;
+    filteredData = data;
 end
