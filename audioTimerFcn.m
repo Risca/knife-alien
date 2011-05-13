5f0f2eb571532eafbf79bce81962cf14449fdf5a
Daniel Josefsson <danjo140@student.liu.se>
2011-05-13 10:51:06 +0200

    Städade bort lite kommentarer. Den förra commiten fungerade inte för Patrik, men det gjorde den för mig. Funktionalliteten nu är betydligt bättre, då man anropar förälder till obj.stemHandle för att få tag i axlarna.
    Signed-off-by: Daniel Josefsson <danjo140@student.liu.se>

--- audioTimerFcn.m
+++ audioTimerFcn.m
@@ -6,24 +6,17 @@ Y = obj.Data;
 handle_graph_input = get(obj.stemHandle, 'Parent');
 xlim = get(handle_graph_input, 'XLim');
 %Find the last intresting index
-maxindex = find(Y > 0.2, 1, 'last');
+maxindex = find(Y > 0.05, 1, 'last');
 if isempty(maxindex)
     maxindex = xlim(end)*(Nfft/fs)/2;
 end
-
 if maxindex < xlim(end)*(Nfft/fs)
     newendindex = floor(xlim(end)*Nfft/fs - floor(0.02*(xlim(end)*Nfft/fs - maxindex)));
 else
-    newendindex = floor(maxindex/(1 - 0.05));
-%     if newendindex > xlim(end)*(Nfft/fs)
-%         newendindex = xlim(end)*(Nfft/fs);
-%     end
+    newendindex = floor(maxindex/(1 - 0.15));
 end
-% Denna rad fuckar upp stemplot
-%set(obj.stemHandle,'XData', 0:newendindex*(fs/Nfft));
+%Sätt ut nya gränser och rita upp ny data.
 set(handle_graph_input, 'XLim', [0 newendindex*(fs/Nfft)]);
 set(handle_graph_input, 'YLim', [0 ceil(max(Y))]);
-% Denna med
-%Y = Y(1:newendindex);
 set(obj.stemHandle,'YData',Y);
 drawnow;
