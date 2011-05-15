a22d15b3de7b476a4b89a3388f8feea45a5ac944
Patrik Dahlström <patda293@student.liu.se>
2011-05-15 15:43:11 +0200

    Removed debugging trash
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- +Filters/insertFilter.m
+++ +Filters/insertFilter.m
@@ -2,6 +2,3 @@ function insertFilter(filter,before,after)
 delete(before.listener);
 before.listener = addlistener(before,'FilteringComplete',@filter.eventHandler);
 filter.listener = addlistener(filter,'FilteringComplete',@after.eventHandler);
-filter
-before
-after
