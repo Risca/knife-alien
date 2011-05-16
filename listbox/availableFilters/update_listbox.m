4fa8b2b2c6c0d1000f0f03023fecfd44c1cbba55
Patrik Dahlström <patda293@student.liu.se>
2011-05-16 11:53:21 +0200

    Hardwired available filters listbos
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- listbox/availableFilters/update_listbox.m
+++ listbox/availableFilters/update_listbox.m
@@ -1,3 +1,14 @@
 function update_listbox(handles)
-vars = evalin('base','who');
-set(handles.listbox_availableFilters,'String',vars);
+%vars = evalin('base','who');
+% Add some filters to listbox
+handles.availableFilters = cell(3,1);
+handles.availableFilters{3} = Filters.HighpassFilter;
+handles.availableFilters{2} = Filters.BandpassFilter;
+handles.availableFilters{1} = Filters.LowpassFilter;
+
+filterNames = cellstr(['foo';'bar';'baz']);
+for k = 1:1:numel(handles.availableFilters)
+    filterNames{k} = handles.availableFilters{k}.Name;
+end
+
+set(handles.listbox_availableFilters,'String',filterNames);
