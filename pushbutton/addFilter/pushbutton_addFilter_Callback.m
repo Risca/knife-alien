5fa73f6265ae425911f1200d3fd449ff76323841
Patrik Dahlström <patda293@student.liu.se>
2011-05-17 15:04:37 +0200

    Fixed bug with first added filter showing up wrong
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- pushbutton/addFilter/pushbutton_addFilter_Callback.m
+++ pushbutton/addFilter/pushbutton_addFilter_Callback.m
@@ -3,15 +3,15 @@ function pushbutton_addFilter_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_addFilter (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-list_entries = get(handles.listbox_availableFilters,'String');
+list_entries = cellstr(get(handles.listbox_availableFilters,'String'));
 index_selected = get(handles.listbox_availableFilters,'Value');
 contents = cellstr(get(handles.listbox_activeFilters,'String'));
-if contents{1}
-    contents = [contents; list_entries{index_selected}];
+if isempty(contents{1})
+    contents = cellstr(list_entries{index_selected});
 else
-    contents = list_entries{index_selected};
+    contents = [contents; list_entries{index_selected}];
 end
 set(handles.listbox_activeFilters,'String', contents);
-set(handles.listbox_activeFilters,'Value', length(contents));
+set(handles.listbox_activeFilters,'Value', numel(contents));
 set(handles.pushbutton_removeFilter, 'Enable', 'on');
 updateMoveFilterButtons(handles);
