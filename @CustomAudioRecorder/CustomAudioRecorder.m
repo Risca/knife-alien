a733ff652fd1f4a5bb09074538fc2e4add6bd46e
Patrik Dahlström <patda293@student.liu.se>
2011-05-17 15:51:55 +0200

    Removed fftData property and added Nfft
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- @CustomAudioRecorder/CustomAudioRecorder.m
+++ @CustomAudioRecorder/CustomAudioRecorder.m
@@ -2,29 +2,29 @@ classdef CustomAudioRecorder < audiorecorder & handle
     properties
         userData
         listener
-        Fs
     end
     properties (SetAccess = protected)
         Data
-        fftData
+        Fs
+        Nfft
     end
     methods
         function obj = CustomAudioRecorder(Fs,nBits,nChannels,Nfft,stemHandle)
             obj = obj@audiorecorder(Fs,nBits,nChannels);
-            obj.fftData.fs = Fs;
-            obj.fftData.Nfft = Nfft;
+            obj.Fs = Fs;
+            obj.Nfft = Nfft;
             obj.userData = stemHandle;
             obj.TimerFcn = @obj.customTimerFcn;
         end
         function customTimerFcn(obj,src,eventData)
-            Nfft = obj.fftData.Nfft;
+            % Nfft = obj.Nfft;
             audioData = getaudiodata(src);
             % Only process last Nfft samples
-            audioData = audioData(end-Nfft:end);
-            audioData = fft(audioData,Nfft);
-            audioData = audioData(1:Nfft/2);
+            audioData = audioData(end-obj.Nfft:end);
+            audioData = fft(audioData,obj.Nfft);
+            audioData = audioData(1:obj.Nfft/2);
             % Adjust magnitude
-            obj.Data = abs(audioData)*2/Nfft;
+            obj.Data = abs(audioData)*2/obj.Nfft;
             notify(obj,'NewAudioData');
         end
     end
