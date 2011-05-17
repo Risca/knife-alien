051e5ca53b401e05c6123b1c0d3b7bc502dc59e6
Patrik Dahlström <patda293@student.liu.se>
2011-05-17 18:09:12 +0200

    Added some error prevention
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- +Filters/insertFilter.m
+++ +Filters/insertFilter.m
@@ -1,11 +1,8 @@
 function insertFilter(filter,before,after)
 % Adjust Next and Prev
-before.Next = filter;
+if ~isempty(before)
+    before.Next = filter;
+end
 filter.Prev = before;
 filter.Next = after;
 after.Prev = filter;
-% Delete listener from filter before
-%% Deprecated!
-% delete(before.listener);
-% before.listener = addlistener(before,'FilteringComplete',@filter.eventHandler);
-% filter.listener = addlistener(filter,'FilteringComplete',@after.eventHandler);
