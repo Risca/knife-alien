dea1aeaaa764bf1544c9c85fee8e1f135be41b69
Daniel Josefsson <danjo140@student.liu.se>
2011-05-17 15:49:59 +0200

    Lade till funktionallitet för att kunna laborera med filter_config-panelen.
    Signed-off-by: Daniel Josefsson <danjo140@student.liu.se>

---
+++ FilterConfig/filter_config_CreateFcn.m
@@ -0,0 +1,7 @@
+% --- Executes during object creation, after setting all properties.
+function filter_config_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to filter_config (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+hObject
+handles
