a23ec32e83ad7002d56eb20d0e351b04fc368454
Daniel Josefsson <danjo140@student.liu.se>
2011-05-19 00:07:51 +0200

    Nu fungerar callbackfunktionen för uppdatering av filterparametrar. Det ändras också direkt i GUI.
    Signed-off-by: Daniel Josefsson <danjo140@student.liu.se>

--- helpers/setFilterParam.m
+++ helpers/setFilterParam.m
@@ -1,8 +1,8 @@
-function [ ret ] = setFilterParam( hObj, event, theFilter, propertyname)
+function setFilterParam( hObj, event, theFilter, propertyname, valuehandle)
 %SETFILTERPARAM Summary of this function goes here
 %   Detailed explanation goes here
-    propertyname
-    ret = 1;
-
+    new_frequency = get(hObj,'Value');
+    set(theFilter, propertyname, new_frequency);
+    set(valuehandle, 'String', num2str(new_frequency));
 end
 
