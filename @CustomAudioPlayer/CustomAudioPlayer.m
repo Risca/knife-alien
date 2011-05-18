165cbd3c3eb7e4f7b4103a9fe883a8f74a948072
Staffan Sjöqvist <stafan87@gmail.com>
2011-05-18 16:25:21 +0200

    Testat lite...
    Signed-off-by: Staffan Sjöqvist <stafan87@gmail.com>

--- @CustomAudioPlayer/CustomAudioPlayer.m
+++ @CustomAudioPlayer/CustomAudioPlayer.m
@@ -1,4 +1,4 @@
-classdef CustomAudioPlayer < timer
+classdef CustomAudioPlayer < timer & handle
 
     properties
         Fs      % Sampling frequency
@@ -20,14 +20,18 @@ classdef CustomAudioPlayer < timer
         % Constructor
         function obj = CustomAudioPlayer( filename, timerInterval )
             obj = obj@timer('ExecutionMode', 'FixedRate',...
-                            'Period', timerInterval, ...
-                            'TimerFcn', @obj.customTimerFcn );
+                            'Period', timerInterval)
+                            
+            obj.TimerFcn = @obj.customTimerFcn
             
             % Open and convert file, do not handle stereo for now
             [y, obj.Fs] = wavread( filename );
-            obj.Audiofile = y;                                             
-                        
-            % Set up timer            
+            obj.Audiofile = y(:,1);                                           
+           
+        end
+        
+        function startAudioPlayer(obj)
+            set( obj, 'Running', 'on' );
         end
         
         function customTimerFcn( obj, src, EventData )
