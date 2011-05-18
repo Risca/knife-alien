861f7efcfbba4408e6e4781939299743ec027d5e
Patrik Dahlström <patda293@student.liu.se>
2011-05-18 21:11:41 +0200

    Pass on Nfft from previus filters
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- +Filters/@BandpassFilter/filter.m
+++ +Filters/@BandpassFilter/filter.m
@@ -1,7 +1,8 @@
     % Filter function for BandpassFilter
 
 function filteredData = filter (obj, data)
-
+    % Pass on previus filters' Nfft
+    obj.Nfft = obj.Prev.Nfft;
     % Data is assumed to be transformed.
     % Sample frequency hard-coded for now
     fs = 22050;
