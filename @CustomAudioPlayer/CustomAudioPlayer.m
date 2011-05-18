876cc68de9f2a7dd4d48d4c2015ec2713e10f305
Staffan Sjöqvist <stafan87@gmail.com>
2011-05-19 01:46:57 +0200

    CustomaudioPlayer funkar kanske...
    Signed-off-by: Staffan Sjöqvist <stafan87@gmail.com>

--- @CustomAudioPlayer/CustomAudioPlayer.m
+++ @CustomAudioPlayer/CustomAudioPlayer.m
@@ -7,12 +7,15 @@ classdef CustomAudioPlayer < handle
     properties (SetAccess = protected)
         Data    % Audiodata
         fftData
+        frame_size  % Number of samples per frame
     end
     
     properties (Access = 'private')
         Audiofile   % All samples of audio file
         Nsamples    % Number of samples in audio file
         timerObj    % Timer
+        position    % starting index of next chunk of data to be processed
+        
     end
     
     
@@ -27,8 +30,12 @@ classdef CustomAudioPlayer < handle
             
             % Open and convert file, do not handle stereo for now
             [y, obj.Fs] = wavread( filename );
-            obj.Audiofile = y(:,1);                                           
-           
+            obj.Audiofile = y(:,1);              
+            
+            % Calculate frame size
+            obj.frame_size = floor( obj.Fs * timerInterval );
+            obj.position = 1;
+            
         end
         
         function start(obj)
@@ -37,10 +44,29 @@ classdef CustomAudioPlayer < handle
         
         function stop(obj)
             stop( obj.timerObj );
+            disp('stop called');
         end
         
         function customTimerFcn( obj, src, EventData )
             disp( 'customTimerFcn Executed');
+            
+        % 1: Plocka ut en lagom bit av filen
+            endIndex = obj.position + obj.frame_size;
+            if endIndex > length(obj.Audiofile)
+                endIndex = length(obj.Audiofile);
+                % End of file reached, stop timer
+                obj.stop();
+            end
+            disp(['StartIndex: ' num2str(obj.position) ' EndIndex: ' num2str(endIndex)]);
+            tempData = obj.Audiofile(obj.position : endIndex);
+            obj.position = endIndex;
+            
+        % 2: Fouriertransformera
+            obj.Data = fft(tempData);
+            obj.Data = obj.Data(1:floor(length(obj.Data)/2));
+            
+        % 3: Let the world know...
+            notify(obj,'NewAudioData');
         end
         
         % Returns true if the player is playing.
