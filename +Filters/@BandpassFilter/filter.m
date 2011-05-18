1bfd2c3c6cfa5d71748b7ed8d865a5a7a68e350f
Patrik Dahlström <patda293@student.liu.se>
2011-05-18 11:08:21 +0200

    Fixed dimension bug
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- +Filters/@BandpassFilter/filter.m
+++ +Filters/@BandpassFilter/filter.m
@@ -13,7 +13,7 @@ function filteredData = filter (obj, data)
     n_low = floor(obj.lowFreq / delta_f);
     n_high = floor(obj.highFreq / delta_f);
 
-    FilteringMask = [zeros(1, n_low-1) ones(1, n_high-n_low+1) zeros(1, length(data)-n_high) ]; 
+    FilteringMask = [zeros(1, n_low-1) ones(1, n_high-n_low+1) zeros(1, length(data)-n_high) ]'; 
 
     % Perform filtering
     obj.Data = data .* FilteringMask;
