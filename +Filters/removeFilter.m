cc0458ad93fe4f30c63bcfd37a01e0d58d9ee95c
Patrik Dahlström <patda293@student.liu.se>
2011-05-18 11:01:01 +0200

    Removed commented out code
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- +Filters/removeFilter.m
+++ +Filters/removeFilter.m
@@ -3,7 +3,3 @@ before = filter.Prev;
 after = filter.Next;
 before.Next = after;
 after.Prev = before;
-%% Deprecated
-% delete(before.listener);
-% delete(filter.listener);
-% before.listener = addlistener(before,'FilteringComplete',@after.eventHandler);
