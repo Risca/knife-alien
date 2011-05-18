168b332d25e89981443250e6f63b764ea9ad4c2c
Daniel Josefsson <danjo140@student.liu.se>
2011-05-19 00:11:34 +0200

    Kommenterad.
    Signed-off-by: Daniel Josefsson <danjo140@student.liu.se>

--- helpers/setFilterParam.m
+++ helpers/setFilterParam.m
@@ -1,6 +1,9 @@
 function setFilterParam( hObj, event, theFilter, propertyname, valuehandle)
-%SETFILTERPARAM Summary of this function goes here
-%   Detailed explanation goes here
+%SETFILTERPARAM Set filter parameters and update filter_config slider values.
+%   event is not used.
+%theFilter is the specific filter that is going to be altered.
+%propertyname is the property that is going to be altered.
+%valuehandle is a handle to the ui that visulizes the property value.
     new_frequency = get(hObj,'Value');
     set(theFilter, propertyname, new_frequency);
     set(valuehandle, 'String', num2str(new_frequency));
