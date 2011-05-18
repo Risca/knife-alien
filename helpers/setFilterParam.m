edabe0f6737752a6b0fb297440ff813ba33001ad
Daniel Josefsson <danjo140@student.liu.se>
2011-05-18 21:57:36 +0200

    Påbörjat arbete för att kunna sätta filteregenskaper. Som det är nu kommer nya sliders upp för varje filter. Dessa gör dock ingenting än.
    Signed-off-by: Daniel Josefsson <danjo140@student.liu.se>

---
+++ helpers/setFilterParam.m
@@ -0,0 +1,7 @@
+function [ output_args ] = setFilterParam( hObj, theFilter, propertyname)
+%SETFILTERPARAM Summary of this function goes here
+%   Detailed explanation goes here
+    propertyname
+
+end
+
