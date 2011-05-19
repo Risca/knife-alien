0a0c451b90071c1df18d7ddeb8e392d8891939b9
Patrik Dahlström <patda293@student.liu.se>
2011-05-19 10:48:30 +0200

    Ghost signals should only be removed prior to presentation. Fixed now
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

--- helpers/saveFilteredAudio.m
+++ helpers/saveFilteredAudio.m
@@ -5,4 +5,4 @@ if exist('recorded_audio.wav','file')
     Y = wavread('recorded_audio.wav');
 end
 Y = [Y; audioData];
-wavwrite(Y,obj.Fs/2,'recorded_audio.wav');
+wavwrite(Y,obj.Fs,'recorded_audio.wav');
