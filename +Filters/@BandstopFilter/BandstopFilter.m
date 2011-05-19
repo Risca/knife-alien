2b118152df6d1f1e84385f90b259a979a568726b
Staffan Sjöqvist <stafan87@gmail.com>
2011-05-19 12:07:41 +0200

    Created Bandstop Filter
    Signed-off-by: Staffan Sjöqvist <stafan87@gmail.com>

---
+++ +Filters/@BandstopFilter/BandstopFilter.m
@@ -0,0 +1,12 @@
+classdef BandpassFilter < Filters.FilterClass & handle & hgsetget
+    properties
+        lowFreq = 1000;
+        highFreq = 5000;
+    end
+    
+    methods
+        function obj = BandpassFilter(obj)
+            obj.Name = 'Bandstop';
+        end
+    end
+end
---
+++ +Filters/@BandstopFilter/filter.m
@@ -0,0 +1,25 @@
+    % Filter function for BandstopFilter
+
+function filteredData = filter (obj, data)
+    % Data is assumed to be transformed.
+    % Sample frequency hard-coded for now
+    fs = 22050;
+      
+    % Calculate which parts of data to set to zero and create filtering mask
+    delta_f = fs / length(data);
+    
+    
+    n_low = floor(obj.lowFreq / delta_f);
+    n_high = floor(obj.highFreq / delta_f);
+
+    FilteringMask = [ones(1, n_low-1) zeros(1, n_high-n_low+1) ones(1, length(data)-n_high) ]'; 
+
+    % Perform filtering
+    obj.Data = data .* FilteringMask;
+    filteredData = obj.Data;
+    % Notify world
+    notify(obj,'FilteringComplete');
+    if ~isempty(obj.Next)
+        obj.Next.filter(obj.Data);
+    end
+end
